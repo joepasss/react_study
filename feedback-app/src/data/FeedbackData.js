940911431f20bedb0dab42c9a0c41d2eaf57408b
Miller <farwes1180@gmail.com>
2022-03-25 17:00:47 +0900

    add FeedbackData (mockup data)

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,29 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectur adipisicing elit.',
+  },
+  {
+    id: 2,
+    rating: 4,
+    text: 'Lorem ipsum dolor sit amet consectur adipisicing elit.',
+  },
+  {
+    id: 3,
+    rating: 6,
+    text: 'Lorem ipsum dolor sit amet consectur adipisicing elit.',
+  },
+  {
+    id: 4,
+    rating: 5,
+    text: 'Lorem ipsum dolor sit amet consectur adipisicing elit.',
+  },
+  {
+    id: 5,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectur adipisicing elit.',
+  },
+];
+
+export default FeedbackData;
