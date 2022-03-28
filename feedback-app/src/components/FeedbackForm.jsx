cda642a9f4534d8ff2491120270abeca35b39637
Miller <farwes1180@gmail.com>
2022-03-28 20:08:20 +0900

    add real time validation in feedbackform

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -5,9 +5,22 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (e) => {
-    console.log(e.target.value);
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 characters');
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+
+    setText(e.target.value);
   };
 
   return (
@@ -16,12 +29,16 @@ function FeedbackForm() {
         <h2>how would you rate your service with us?</h2>
         <div className='input-group'>
           <input
+            onChange={handleTextChange}
             type='text'
             placeholder='Write a review'
-            onChange={handleTextChange}
+            value={text}
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
