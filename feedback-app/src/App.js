fb1102893071195838853214d70611d587591f29
Miller <farwes1180@gmail.com>
2022-03-31 04:01:19 +0900

    useParams

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,16 +1,16 @@
 import { v4 as uuidv4 } from 'uuid';
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { useState } from 'react';
 
 import AboutPage from './pages/AboutPage';
 
-import Card from './components/shared/Card';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutIconLink from './components/AboutIconLink';
+import Post from './components/Post';
 
 import FeedbackData from './data/FeedbackData';
 
@@ -48,16 +48,8 @@ function App() {
             }
           />
           <Route path='/about' element={<AboutPage />} />
+          <Route path='/post/:id/:name' element={<Post />} />
         </Routes>
-
-        <Card>
-          <NavLink to='/' activeClassName='active'>
-            Home
-          </NavLink>
-          <NavLink to='/about' activeClassName='active'>
-            About
-          </NavLink>
-        </Card>
       </div>
     </BrowserRouter>
   );
