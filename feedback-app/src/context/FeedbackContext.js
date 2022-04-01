e8c88869f9a6ee70d1492f537c3e0f85e7fb6975
Miller <farwes1180@gmail.com>
2022-04-01 14:38:24 +0900

    feedback edit

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -8,22 +8,36 @@ const FeedbackContext = createContext();
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
+
+  // delete Feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?'))
       setFeedback(feedback.filter((item) => item.id !== id));
   };
 
+  // add Feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
+  };
+
+  // Set item to be update
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
   };
 
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}
