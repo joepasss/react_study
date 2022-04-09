9a7346e8eedfedf2cb54e0fc4f66aa55053c3626
Miller <farwes1180@gmail.com>
2022-04-10 05:32:58 +0900

    Add clear users method

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -36,12 +36,23 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  // Clear users from state
+  const clearUsers = () =>
+    dispatch({
+      type: 'CLEAR_USERS',
+    });
+
   // set Loading
   const setLoading = () => dispatch({ type: 'SET_LOADING ' });
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, searchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
