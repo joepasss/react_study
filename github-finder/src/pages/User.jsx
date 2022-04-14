b74431ac74fab444bc361bf8c6639e65ab253dd6
Miller <farwes1180@gmail.com>
2022-04-14 22:06:31 +0900

    Repo Items

--- github-finder/src/pages/User.jsx
+++ github-finder/src/pages/User.jsx
@@ -16,6 +16,7 @@ function User() {
   useEffect(() => {
     getUser(params.login);
     getUserRepos(params.login);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const {
