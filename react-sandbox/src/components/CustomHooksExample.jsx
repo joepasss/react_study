bc5e6791de42b1dbe231aa5356f567a4018b54e3
Miller <farwes1180@gmail.com>
2022-04-17 21:58:59 +0900

    implement to useState (before making customhook)

--- react-sandbox/src/components/CustomHooksExample.jsx
+++ react-sandbox/src/components/CustomHooksExample.jsx
@@ -1,21 +1,39 @@
-import useFetch from '../hooks/useFetch';
+import { useState } from 'react';
 
 function CustomHooksExample() {
-  const { data, loading } = useFetch(
-    'https://jsonplaceholder.typicode.com/posts',
-    {}
-  );
+  // const { data, loading } = useFetch(
+  //   'https://jsonplaceholder.typicode.com/posts',
+  //   {}
+  // );
 
-  if (loading) {
-    return <h3>Loading ...</h3>;
-  }
+  // if (loading) {
+  //   return <h3>Loading ...</h3>;
+  // }
+
+  // return (
+  //   <div>
+  //     {data.map((post) => (
+  //       <h3 key={post.id}>{post.title}</h3>
+  //     ))}
+  //   </div>
+  // );
+
+  const [task, setTask] = useState('');
 
   return (
-    <div>
-      {data.map((post) => (
-        <h3 key={post.id}>{post.title}</h3>
-      ))}
-    </div>
+    <form className='w-50'>
+      <div className='mb-3'>
+        <label htmlFor='' className='form-label'>
+          Task
+        </label>
+        <input
+          className='form-control'
+          type='text'
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+      </div>
+    </form>
   );
 }
 
