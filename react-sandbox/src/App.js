e0198eb65bb621701b73bb666d3a9b1ef9fa6d81
Miller <farwes1180@gmail.com>
2022-04-18 19:16:05 +0900

    useRef Example 1 - Create DOM Reference

--- react-sandbox/src/App.js
+++ react-sandbox/src/App.js
@@ -1,5 +1,11 @@
+import UseRefExample1 from './components/UseRefExample1';
+
 function App() {
-  return <div className='App'>APP</div>;
+  return (
+    <div id='App' className='container mt-5'>
+      <UseRefExample1 />
+    </div>
+  );
 }
 
 export default App;
