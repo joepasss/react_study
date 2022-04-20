c1277ba1b457d08eec2bdee5abfd97cdd51027b2
Miller <farwes1180@gmail.com>
2022-04-20 16:49:16 +0900

    House MarketPlace app initialize

---
+++ house-marketplace/src/firebase.config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFireStore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCJ5ZGHM5Qzx_HONajiFl8SOtF2o-pAUpA',
+  authDomain: 'house-marketplace-app-bd010.firebaseapp.com',
+  projectId: 'house-marketplace-app-bd010',
+  storageBucket: 'house-marketplace-app-bd010.appspot.com',
+  messagingSenderId: '578694022553',
+  appId: '1:578694022553:web:1cbb21e28bb6c01fc4bf08',
+  measurementId: 'G-8NDJNWVSN9',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFireStore();
