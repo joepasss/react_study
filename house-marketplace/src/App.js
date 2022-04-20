3d835c200b69c58764bcf0fde15d953cb21b735c
Miller <farwes1180@gmail.com>
2022-04-21 03:51:21 +0900

    PrivateRoute Component & useAuthStatus Hook

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 // import components
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 
 // import Pages
 import Explore from './pages/Explore';
@@ -21,7 +22,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
