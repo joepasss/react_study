7c25eb97ac2a1dd625e39e96626991ef35eec133
Miller <farwes1180@gmail.com>
2022-04-29 21:40:19 +0900

    Listings Page Slider

--- house-marketplace/src/pages/Listing.jsx
+++ house-marketplace/src/pages/Listing.jsx
@@ -2,6 +2,9 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 // firebase
 import { getDoc, doc } from 'firebase/firestore';
@@ -13,6 +16,8 @@ import Spinner from '../components/Spinner';
 
 // Icons
 import shareIcon from '../assets/svg/shareIcon.svg';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 function Listing() {
   const [listing, setListing] = useState(null);
@@ -44,7 +49,25 @@ function Listing() {
 
   return (
     <main>
-      {/* SLIDER */}
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imageUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                // background: `url(${listing.imageUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className='swiperSlideDiv'
+            >
+              <img
+                src={listing.imageUrls[index]}
+                alt='img'
+                className='swiperSlideImg'
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div
         className='shareIconDiv'
