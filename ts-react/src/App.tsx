78c1bf7f8bb18839545187a398b429095bc6bd02
Miller <farwes1180@gmail.com>
2022-05-01 10:02:30 +0900

    Context API in typescript-react

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,17 +1,33 @@
-import React, { FC } from 'react';
+import React, { FC, createContext } from 'react';
 import './App.css';
 import { Person, HairColor } from './components/Person';
 
+interface AppContextInterface {
+  name: string;
+  age: number;
+  country: string;
+}
+
+const AppContext = createContext<AppContextInterface | null>(null);
+
 const App: FC = () => {
+  const contextValue: AppContextInterface = {
+    name: 'JOE',
+    age: 69,
+    country: 'josun',
+  };
+
   return (
-    <div className='App'>
-      <Person
-        name='joe'
-        age={69}
-        email='joe@gmail.com'
-        hairColor={HairColor.Blonde}
-      />
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className='App'>
+        <Person
+          name='joe'
+          age={69}
+          email='joe@gmail.com'
+          hairColor={HairColor.Blonde}
+        />
+      </div>
+    </AppContext.Provider>
   );
 };
 
