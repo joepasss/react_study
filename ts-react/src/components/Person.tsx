365de4d3934bfa02f6e7f3d7b070412657e835fa
Miller <farwes1180@gmail.com>
2022-05-01 09:51:40 +0900

    UseState in typescript-react

--- ts-react/src/components/Person.tsx
+++ ts-react/src/components/Person.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 
 interface Props {
   name: string;
@@ -8,12 +8,26 @@ interface Props {
 }
 
 export const Person: FC<Props> = ({ name, age, email }) => {
+  // const [country, setCountry] = useState(''); JS
+  const [country, setCountry] = useState<string | null>(null);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCountry(event.target.value);
+  };
+
   return (
     <div>
       <h1>
         {name} <span>{age}</span>
       </h1>
       <h2>@ {email}</h2>
+
+      <input
+        placeholder='Write down your country ...'
+        onChange={handleChange}
+      />
+      <br />
+      {country}
     </div>
   );
 };
