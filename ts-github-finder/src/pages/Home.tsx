c3f00d1494648a47bac94157c904d96c00bac4e2
Miller <farwes1180@gmail.com>
2022-05-07 16:51:47 +0900

    Add User Search Component

--- ts-github-finder/src/pages/Home.tsx
+++ ts-github-finder/src/pages/Home.tsx
@@ -2,12 +2,13 @@ import { FC } from 'react';
 
 // Components
 import { UserResults } from '../component/users/UserResults';
+import { UserSearch } from '../component/users/UserSearch';
 
 export const Home: FC = () => {
   return (
-    <div>
-      {/* SEARCH COMPONENT */}
+    <>
+      <UserSearch />
       <UserResults />
-    </div>
+    </>
   );
 };
