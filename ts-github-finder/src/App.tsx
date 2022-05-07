e376271c28f2a472a402328cbacc01055dcae2a2
Miller <farwes1180@gmail.com>
2022-05-07 10:41:41 +0900

    init tailwindcss & daisyUI

---
+++ ts-github-finder/src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+export const App: FC = () => {
+  return (
+    <div className='App'>
+      <h1 className='text-3xl font-bold underline'>HELLO WORLD!</h1>
+    </div>
+  );
+};
+
+export default App;
