9a07b49d151598ba1467b6f82116435d5486897e
Miller <farwes1180@gmail.com>
2022-05-07 10:45:08 +0900

    App init

--- ts-github-finder/src/App.tsx
+++ ts-github-finder/src/App.tsx
@@ -1,10 +1,18 @@
 import { FC } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { Navbar } from './component/layout/Navbar';
 
 export const App: FC = () => {
   return (
-    <div className='App'>
-      <h1 className='text-3xl font-bold underline'>HELLO WORLD!</h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main>
+          <h1 className='text-3xl font-bold underline'>HELLO WORLD!</h1>
+        </main>
+      </div>
+    </Router>
   );
 };
 
