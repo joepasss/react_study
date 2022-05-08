4da917697aeacfe41226d9c1c6ff64dd1d2fefb1
Miller <farwes1180@gmail.com>
2022-05-08 19:24:52 +0900

    ts-hooks init

---
+++ ts-hooks/src/App.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+import { UseState } from './components/state/UseState';
+
+export const App: FC = () => {
+  return <div className='App'></div>;
+};
---
+++ ts-hooks/src/components/state/UseState.tsx
@@ -0,0 +1,11 @@
+export const UseState = () => {
+  const handleLogin = () => {};
+  const handleLogout = () => {};
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>LogOut</button>
+    </div>
+  );
+};
